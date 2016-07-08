2335313cdc2fda2d5b95fd1d336e7907b5a009bd
Sandor Dargo <sandor.dargo@gmail.com>
2016-07-08 09:00:21 +0200

    refactor: Generalize getPart() in  Roman numerals

--- roman-numerals/src/main/java/RomanNumeral.java
+++ roman-numerals/src/main/java/RomanNumeral.java
@@ -27,42 +27,38 @@ public class RomanNumeral {
     String roman = "";
     Pair<String, Integer> resultPair = new Pair<>("", this.arabicNumber);
     for (Map.Entry<Integer, String> entry : RomanNumeral.arabicRomanMap.entrySet()) {
-      System.out.println(entry.getKey());
-      resultPair = getPart(resultPair.getValue(), entry.getKey(), entry.getValue());
+      resultPair = getPart(resultPair.getValue(), new Pair<Integer, String>(entry.getKey(), entry.getValue()));
       roman += resultPair.getKey();
     }
     return roman;
   }
 
-  private Pair<String, Integer> getPart(int input, int divider, String roman) {
+  private Pair<String, Integer> getPart(int input, Pair<Integer, String> arabicRomanPair) {
     String output = "";
+    int divider = arabicRomanPair.getKey();
     int residual = input;
     int times = residual / divider;
 
     residual = residual % divider;
-    if (times == 1 && ((residual + 1) / divider == 1) && divider == 5) {
-      output += "IX";
-      residual -= 4;
-    } else if (times == 1 && ((residual + 10) / divider == 1) && divider == 50) {
-      output += "XC";
-      residual -= 40;
-    } else if (times == 1 && ((residual + 100) / divider == 1) && divider == 500) {
-      output += "CM";
-      residual -= 400;
+    if (isDivider5or50or500(divider) && times == 1 && ((residual + divider / 5) / divider == 1)) {
+      residual -= (divider / 5) * 4;
+      output += RomanNumeral.arabicRomanMap.get(divider / 5) + RomanNumeral.arabicRomanMap.get(divider * 2);
+    } else if (isDivider1or10or100(divider) && times == 4) {
+        output += RomanNumeral.arabicRomanMap.get(divider) + RomanNumeral.arabicRomanMap.get(5 * divider);
     } else {
-
-      if (divider == 100 && times == 4) {
-        output += "CD";
-      } else if (divider == 10 && times == 4) {
-        output += "XL";
-      } else if (divider == 1 && times == 4) {
-        output += "IV";
-      } else {
         for (int i = 0; i < times; i++) {
-          output += roman;
-        }
+          output += arabicRomanPair.getValue();
       }
     }
     return new Pair<>(output, residual);
   }
+
+  private boolean isDivider5or50or500(int divider) {
+    return (divider == 5 || divider == 50 || divider == 500);
+  }
+
+  private boolean isDivider1or10or100(int divider) {
+    return (divider == 1 || divider == 10 || divider == 100);
+  }
+
 }
